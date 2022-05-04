9fc9b2ac17a0768574f66a1702e6a846258980e9
sarah <sbellemare99@gmail.com>
2022-05-04 14:46:23 -0400

    enabling chat

--- index.js
+++ index.js
@@ -15,12 +15,44 @@ app.get('/', function(req, res){
 	res.send('Hello! I am a research helper. What would you like to share?')
 })
 
+token = "EABK5DqeVQKcBAP1Vo57lcbAnWu5mGcYLrMBfXpaFzO3LJLVII4UEgYdEkGC686fbABbY8y2TLSRia0mI4yJ4vReyHADPZBEVbt8dcjp2C3fVZCVnsxBO1T0BYAmpUNM7ni69OGdbV6GACC9lhOTi7e5IjHN3ZChLFlp9jYCTYIwGQUze12XM1gFh6mZBDSnDisZB7ZAY0ZBegZDZD"
+
 app.get('/webhook/', function(req, res) {
 	if (req.query['hub.verify_token'] === "moose"){
 		res.send(req.query['hub.challenge'])
 	}
 	res.send("Wrong token")
 })
+
+app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging_events
+	for (let i = 0; i < messaging_events.length; i++){
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text){
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0, 100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender, text){
+	let messageData = {text: text}
+	request({
+		url : "https://graph.facebook.com/v2.6/me/messages",
+		qs: {access_token, token},
+		method: "POST",
+		json: {
+			reciept: {id: sender},
+			message: messageData,
+		}, function(error, response, body){
+			console.log("sending error")
+		} else if (response.body.error){
+			console.log("response body error")
+		}
+	})
+}
 
 app.listen(app.get('port'), function(){
 	console.log("running: port")
