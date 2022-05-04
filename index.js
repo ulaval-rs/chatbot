cb5907f6157294257ca703c59b2e380a5f59502d
sarah <sbellemare99@gmail.com>
2022-05-04 14:52:21 -0400

    enabling chat

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get('/', function(req, res){
 	res.send('Hello! I am a research helper. What would you like to share?')
 })
 
+
 token = process.env.TOKEN
 
 app.get('/webhook/', function(req, res) {
@@ -25,7 +26,7 @@ app.get('/webhook/', function(req, res) {
 })
 
 app.post('/webhook/', function(req, res) {
-	let messaging_events = req.body.entry[0].messaging_events
+	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++){
 		let event = messaging_events[i]
 		let sender = event.sender.id
@@ -41,10 +42,10 @@ function sendText(sender, text){
 	let messageData = {text: text}
 	request({
 		url : "https://graph.facebook.com/v2.6/me/messages",
-		qs: {access_token, token},
+		qs: {access_token: token},
 		method: "POST",
 		json: {
-			reciept: {id: sender},
+			recipient: {id: sender},
 			message: messageData,
 		}, function(error, response, body){
 			console.log("sending error")
