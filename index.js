f208d08aa1130257d94db7375243c42f0bbca4d5
sarah <sbellemare99@gmail.com>
2022-05-04 13:44:00 -0400

    init chatbot

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+app.set('port', (process.env.PORT || 5000))
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+	res.send('Hello! I am a research helper. What would you like to share?')
+})
+
+app.get('/webhook', function(req, res) {
+	if (req.query['hub.verify_token'] == "moose"){
+		res.send(req.qery['hub.challenge'])
+	}
+	res.send("Wrong token")
+})
+
+app.listen(app.get('port'), function(){
+	console.log("running: port")
+})
