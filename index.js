0bea20c25cc7a8562899947ff83719cce3c95583
sarah <sbellemare99@gmail.com>
2022-05-09 13:38:18 -0400

    slicing data

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ app.post('/webhook/', function(req, res) {
 				else if (text.indexOf("google") !== -1){
 				let coordinates = text.slice(text.indexOf("@") + 1)
 				if(coordinates.indexOf("z") !== 0){
-					sendText(sender, "data found!")
 					coordinates = coordinates.slice(0, coordinates.indexOf("z") + 1)
 				}
 				let x_and_y_coordinates = coordinates.slice(0, -4)
